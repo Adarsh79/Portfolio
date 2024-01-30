964e12dcbb8012fd1c4cc5f48debc6a238d82e5b
Adarsh Bhaskar <Adarsh.bhaskar7903@gmail.com>
2024-01-30 14:11:30 +0000

    Modified Home Page and About sections

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Adarsh Bhaskar",
+  title: "Machine Learning Engineer & Cloud Enthusiast",
+  email: "adarsh.bhaskar7903@gmail.com",
+  gitHub: "Adarsh79",
+  instagram: "adarsh_0793",
+  linkedIn: "adarsh-b-a",
+  medium: "adarsh.bhaskar7903",
+  twitter: "AdarshB79",
+  youTube: "AdarshBhaskar",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a computer science student studying at Dayananda Sagar Academy of Technology and Management. I enjoy playing around with machine learning algorithms hoping to find inspiration to create something unique.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,11 +32,11 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Machine Learning",
+  "Github",
+  "Cloud Computing",
+  "Data Visualization",
+  "Watching Anime?",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my Machine Learning experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
